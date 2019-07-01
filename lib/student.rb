828b0495b40d78cae0a0bdf62341cbb2549f58b7
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-01 22:39:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,7 +76,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(count)
+  def self.first_X_students_in_grade_10(LIMIT)
     sql = <<-SQL
       SELECT * FROM students WHERE grade = "10" LIMIT 2
     SQL
