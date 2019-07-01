4a474473b174aa474aa8c20c33bf71a669b11915
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-01 22:05:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,9 +76,9 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(count)
+  def self.first_X_students_in_grade_10
     sql = <<-SQL
-      SELECT COUNT(?) FROM students WHERE grade = "10"
+      SELECT * FROM students WHERE grade = "10"
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
