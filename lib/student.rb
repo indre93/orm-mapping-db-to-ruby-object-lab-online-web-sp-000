92c2cc94d215f7d935ef735264be0c2987485232
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-01 23:04:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,7 +89,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 10
     SQL
-    DB[:conn].execute(sql)first
+    DB[:conn].execute(sql).first
   end
 
   def self.all_students_in_grade_X(grade)
