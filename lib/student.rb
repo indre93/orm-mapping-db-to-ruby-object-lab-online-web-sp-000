ebe5d4f77e5b1b8f6572663e776039f31d42149e
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-01 21:07:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,7 +82,7 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = "9"
+      SELECT * FROM students WHERE grade = ?
     SQL
     DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
