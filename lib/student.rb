a1986c33b3553f825d3e08521da706c22080ed10
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-01 21:06:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,12 @@ class Student
   end
 
   def self.first_student_in_grade_10
-
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = "9"
+    SQL
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.all_students_in_grade_X
