0a94a36cefeace9c6e124d21971cdbcab0125894
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-01 22:13:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,7 +76,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(id)
+  def self.first_X_students_in_grade_10(grade)
     sql = <<-SQL
       SELECT * FROM students WHERE grade = ?
     SQL
